2e22c1f4bc17159b2d8f769b9191f4da3da9360b
patilash19 <patilashish192005@gmail.com>
2025-05-16 22:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import PyPDF2
 import io
 import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
 
 load_dotenv()
 
